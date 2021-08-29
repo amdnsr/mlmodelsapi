d66f7e332da5f15e0970e98bb50abc0c4a937f7b
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-08-30 02:27:35 +0530

    Added config files

---
+++ config/config.py
@@ -0,0 +1,9 @@
+import json
+
+class Configuration:
+    def __init__(self, config_file_path):
+        self.config_file_path = config_file_path
+        self._load_config()
+        
+    def _load_config(self):
+        self.config_json = json.load(self.config_file_path)
